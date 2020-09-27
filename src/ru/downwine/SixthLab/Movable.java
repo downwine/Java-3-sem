bfcf150ec5baac9e512595ce40db80375743227b
Ksusha <aleks.laksu69@gmail.com>
2020-09-27 21:57:13 +0300

    update 6 lab

---
+++ src/ru/downwine/SixthLab/Movable.java
@@ -0,0 +1,8 @@
+package ru.downwine.SixthLab;
+
+public interface Movable {
+    public abstract void moveUp();
+    public abstract void moveDown();
+    public void moveLeft();
+    public void moveRight();
+}
