43483c6e05594e4a021bd685c08ec2f6f34555e0
Ksusha <aleks.laksu69@gmail.com>
2020-10-03 14:34:27 +0300

    7 lab ex3

---
+++ src/ru/downwine/SeventhLab/Ex3/Chair.java
@@ -0,0 +1,44 @@
+package ru.downwine.SeventhLab.Ex3;
+
+import java.util.Scanner;
+
+public class Chair extends Furniture{
+    private boolean occupied;
+
+    public Chair(String klass) {
+        super(klass);
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    @Override
+    public void des() {
+        System.out.println("Сесть на стул? y/n");
+        Scanner in = new Scanner(System.in);
+        switch (in.next()) {
+            case "y": {
+                System.out.println("Вы сели на стул");
+                setOccupied(true);
+                break;
+            }
+            case "n": {
+                System.out.println("Вы не сели на стул");
+                setOccupied(false);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Chair {" +
+                "occupied = " + occupied +
+                '}';
+    }
+}
---
+++ src/ru/downwine/SeventhLab/Ex3/Wardrobe.java
@@ -0,0 +1,42 @@
+package ru.downwine.SeventhLab.Ex3;
+import java.util.Scanner;
+public class Wardrobe extends Furniture {
+    private boolean state;
+
+    public Wardrobe(String klass) {
+        super(klass);
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    @Override
+    public void des() {
+        System.out.println("Открыть шкаф? y/n");
+        Scanner in = new Scanner(System.in);
+        switch (in.next()) {
+            case "y": {
+                System.out.println("Вы открыли шкаф");
+                setState(true);
+                break;
+            }
+            case "n": {
+                System.out.println("Вы не стали открывать шкаф");
+                setState(false);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Wardrobe {" +
+                "state = " + state +
+                '}';
+    }
+}
